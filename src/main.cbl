dcd5ec9e0b5b1a07a3d5246f6073ef5bfb27c1d7
theluqmn <luqman.yazrie@outlook.com>
2025-12-30 23:22:37 +0800

    added ANSI colours

--- src/main.cbl
+++ src/main.cbl
@@ -8,12 +8,16 @@
       *ansi colours
        01 ESC                                  PIC X VALUE X'1B'.
        01 RES                                  PIC X(3) VALUE "[0m".
+       01 GRY                                  PIC X(4) VALUE "[30m".
+       01 UND                                  PIC X(6) VALUE "[4;97m".
+       01 BLD                                  PIC X(6) VALUE "[1;37m".
+       01 RED                                  PIC X(4) VALUE "[31m".
+       01 BLU                                  PIC X(4) VALUE "[34m".
       *temporary variables
        01 TP-STR-A                             PIC X(32).
-       
 
        PROCEDURE DIVISION.
-       DISPLAY "hello from cobol ahh".
+       DISPLAY ESC UND "Timecard" ESC RES.
        PERFORM MAIN.
        CLI-HANDLER.
            DISPLAY "> " WITH NO ADVANCING.
@@ -21,9 +25,9 @@
            MOVE FUNCTION LOWER-CASE(TP-STR-A) TO CLI-INPUT.
 
            IF CLI-INPUT = "exit" THEN
-               DISPLAY "exiting..."
+               DISPLAY ESC BLU "exiting..." ESC RES
            ELSE
-               DISPLAY "unknown command."
+               DISPLAY ESC RED "unknown command." ESC RES
            END-IF.
        MAIN.
            PERFORM CLI-HANDLER UNTIL CLI-INPUT = "exit".
