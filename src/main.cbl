779c845ca795a0164290464dda192f4860f17052
theluqmn <luqman.yazrie@outlook.com>
2025-12-30 22:38:06 +0800

    added CLI-HANDLER

--- src/main.cbl
+++ src/main.cbl
@@ -2,8 +2,30 @@
        PROGRAM-ID. Timecard.
        AUTHOR. theluqmn.
 
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 CLI-INPUT                            PIC X(32).
+      *ansi colours
+       01 ESC                                  PIC X VALUE X'1B'.
+       01 RES                                  PIC X(3) VALUE "[0m".
+      *temporary variables
+       01 TP-STR-A                             PIC X(32).
+       
+
        PROCEDURE DIVISION.
        DISPLAY "hello from cobol ahh".
-       STOP RUN.
+       PERFORM MAIN.
+       CLI-HANDLER.
+           DISPLAY "> " WITH NO ADVANCING.
+           ACCEPT TP-STR-A.
+           MOVE FUNCTION LOWER-CASE(TP-STR-A) TO CLI-INPUT.
+
+           IF CLI-INPUT = "exit" THEN
+               DISPLAY "exiting..."
+           ELSE
+               DISPLAY "unknown command."
+           END-IF.
+       MAIN.
+           PERFORM CLI-HANDLER UNTIL CLI-INPUT = "exit".
+           STOP RUN.
        END PROGRAM Timecard.
-       
