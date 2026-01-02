65322b1a2a3f619b147c8956ad61c863c048ba6b
theluqmn <luqman.yazrie@outlook.com>
2026-01-02 22:08:00 +0800

    started work on initialisation

---
+++ src/temp.cbl
@@ -0,0 +1,86 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Timecard.
+       AUTHOR. theluqmn.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 CLI-INPUT                            PIC X(32).
+      *ANSI FORMATTING
+       01 ESC PIC X(2) VALUE X'1B5B'.  01 RES PIC X(2) VALUE "0m".
+       01 UND PIC X(2) VALUE "4;".     01 BLD PIC X(2) VALUE "1;".
+       01 BLC PIC X(3) VALUE "30m".    01 WHT PIC X(3) VALUE "37m".
+       01 RED PIC X(3) VALUE "31m".    01 BLU PIC X(3) VALUE "34m".
+       01 GRY PIC X(3) VALUE "30m".
+       01 BG-WHT PIC X(3) VALUE "47m".
+      *TEMPORARY VARIABLES
+       01 TP-STR-A                             PIC X(32).
+
+       PROCEDURE DIVISION.
+       CALL "SYSTEM" USING "clear".
+       DISPLAY ESC BG-WHT ESC BLD BLC "TIMECARD" ESC RES.
+       DISPLAY "A CLI-based task management system built using COBOL!".
+       DISPLAY " ".
+       DISPLAY "Basics: 'setup', 'help', 'exit'.".
+       PERFORM MAIN.
+       
+       CLI-HANDLER.
+           DISPLAY "> " WITH NO ADVANCING.
+           ACCEPT TP-STR-A.
+           CALL "SYSTEM" USING "clear".
+           MOVE FUNCTION LOWER-CASE(TP-STR-A) TO CLI-INPUT.
+
+           IF CLI-INPUT = "exit" THEN
+               DISPLAY ESC BLU "Exiting..." ESC RES
+           ELSE IF CLI-INPUT  = "help" THEN
+               PERFORM SCREEN-HELP
+           ELSE
+               DISPLAY ESC RED "Unknown command!" ESC RES
+           END-IF.
+
+       SCREEN-HELP.
+           PERFORM BORDER-TOP.
+           DISPLAY "│ " ESC UND BLD WHT "HELP" ESC RES
+           "                                                          "
+           "│".
+           PERFORM BORDER-EMPTY.
+           DISPLAY
+           "│ GitHub: https://github.com/theluqmn/timecard     "
+           ESC RES
+           "             │".
+           PERFORM BORDER-EMPTY.
+           DISPLAY 
+           "│ - 'help'           - Displays this message       "
+           "             │".
+           DISPLAY 
+           "│ - 'exit'           - Exits the program           "
+           "             │".
+           PERFORM BORDER-BOT.
+
+       BORDER-TOP.
+           DISPLAY "┌" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "┐".
+       BORDER-BOT.
+           DISPLAY "└" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "─────────" WITH NO ADVANCING.
+           DISPLAY "┘".
+       BORDER-EMPTY.
+           DISPLAY "│                                        "
+           "                       │".
+
+       MAIN.
+           PERFORM CLI-HANDLER UNTIL CLI-INPUT = "exit".
+           STOP RUN.
+       END PROGRAM Timecard.
