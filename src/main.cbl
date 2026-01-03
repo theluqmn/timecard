deda12df7c158a029e06e7719c3ac21155cf89ac
theluqmn <luqman.yazrie@outlook.com>
2026-01-03 11:48:09 +0800

    added a CLI header

--- src/main.cbl
+++ src/main.cbl
@@ -2,6 +2,7 @@
        PROGRAM-ID. TIMECARD.
        AUTHOR. theluqmn.
 
+
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
@@ -11,6 +12,7 @@
                RECORD KEY IS TASK-ID
                FILE STATUS IS SFS-TASK.
 
+
        DATA DIVISION.
        FILE SECTION.
        FD TASK-FILE.
@@ -19,6 +21,7 @@
            05 TASK-DETAILS             PIC X(48).
            05 TASK-DATE                PIC 9(8).
            05 TASK-STATUS              PIC 9(1).
+
        WORKING-STORAGE SECTION.
       *SYSTEM VARIABLES
        01 CLI-INPUT PIC X(48).         01 CLI-ACCEPT PIC X(48).
@@ -28,8 +31,10 @@
            05 SYS-CURRENT-YEAR         PIC 9(4).
            05 SYS-CURRENT-MONTH        PIC 9(2).
            05 SYS-CURRENT-DAY          PIC 9(2).
+
       *TEMPORARY VARIABLES
        01 TP-STR-A PIC X(48).          01 TP-STR-B PIC X(48).
+
       *ANSI FORMATTING
        01 ESC PIC X(2) VALUE X'1B5B'.  01 RES PIC X(2) VALUE "0m".
        01 UND PIC X(2) VALUE "4;".     01 BLD PIC X(2) VALUE "1;".
@@ -38,6 +43,7 @@
        01 GRY PIC X(3) VALUE "30m".    01 GRN PIC X(3) VALUE "32m".
        01 YEL PIC X(3) VALUE "33m".
        01 BG-WHT PIC X(3) VALUE "47m".
+
 
        PROCEDURE DIVISION.
        CALL "CLI-CLEAR".
@@ -51,7 +57,7 @@
            DISPLAY "Initialising...".
            ACCEPT SYS-CURRENT-DATE FROM DATE YYYYMMDD.
            DISPLAY ESC BLU "[i] Current date: "
-           SYS-CURRENT-YEAR "-" SYS-CURRENT-MONTH "-" SYS-CURRENT-DAY
+           SYS-CURRENT-YEAR "/" SYS-CURRENT-MONTH "/" SYS-CURRENT-DAY
            ESC RES.
            
            OPEN INPUT TASK-FILE.
@@ -59,13 +65,13 @@
                DISPLAY ESC YEL "[!] Task file not found." ESC RES
                OPEN OUTPUT TASK-FILE
                CLOSE TASK-FILE
-               DISPLAY ESC GRN "[/] A task file was created." ESC RES
+               DISPLAY ESC GRN "[/] Task file was created." ESC RES
            ELSE
                DISPLAY ESC BLU "[i] Task file located." ESC RES
            END-IF.
            CLOSE TASK-FILE.
 
-           DISPLAY "Timecard is ready".
+           DISPLAY ESC GRN "Timecard is ready" ESC RES.
            DISPLAY " ".
 
        CLI-HANDLER.
@@ -75,17 +81,20 @@
            CALL "CLI-CLEAR".
 
            IF CLI-INPUT = "exit" THEN
-               DISPLAY ESC BLU "Exiting..." ESC RES
+               DISPLAY ESC BLU "[i] Exiting..." ESC RES
            ELSE IF CLI-INPUT  = "help" THEN
                PERFORM SCREEN-HELP
            ELSE
-               DISPLAY ESC RED "Unknown command!" ESC RES
+               DISPLAY ESC RED "[!] Unknown command!" ESC RES
            END-IF.
+       
+       CLI-HEADER.
+           DISPLAY "  Timecard                                        "
+           "   "
+           SYS-CURRENT-YEAR "/" SYS-CURRENT-MONTH "/" SYS-CURRENT-DAY.
 
        SCREEN-HELP.
-           DISPLAY "  Timecard                                        "
-           "  "
-           SYS-CURRENT-YEAR "-" SYS-CURRENT-MONTH "-" SYS-CURRENT-DAY.
+           PERFORM CLI-HEADER.
            CALL "BORDER-TOP".
            DISPLAY "│ " ESC UND BLD WHT "HELP" ESC RES
            "                                                          "
